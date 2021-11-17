a6a4eca29d39683a4205994d75ba03bacff83c0a
a.efimov <a.efimov@rdp.ru>
2021-11-17 19:40:46 +0300

    [A] push back method added

--- container.hpp
+++ container.hpp
@@ -17,25 +17,50 @@ class Container
         if(capacity >= n) return;
         T* newarr = reinterpret_cast<T*>(new uint8_t[ n*sizeof(T) ]);
         //placment new
-        for(size_t i; i < size; ++i)
+        try {
+            for(size_t i; i < size; ++i)
+            {
+                new (newarr + i)T(arr[i]);
+            }
+        }
+        catch(...)
         {
-            new (newarr +i)T(arr[i]);
+            for(size_t j=i-1; j >=0; --j)
+            {
+                (newarr + i)->~T();
+            }
+            delete[] reinteret_cast<uint8_t>(newarr);
+            throw;
         }
         for(size_t i=0; i < size; ++i)
         {
-            arr[i].~T();
+            (arr + i)->~T();
         }
         delete[] reinterpret_cast<uint8_t*>(arr);
         arr = newarr;
         capacity = n;
     }
 
-    void resize(size_t n)
+    void resize(size_t n, T value = T())
     {        
         if(n > capacity) reserve(n);
         size = n;
+
+        // insert default data
     }
 
-    void push_back(const T& value) {};
+    void push_back(const T& value)
+    {
+        if (size >= capacity) resize(size*2);
+        try {
+            new (arr + size) value;
+        }
+        catch(...)
+        {
+            // Shoud I do something?
+            throw;
+        }
+        ++size;        
+    }
     void pop_back() {};
 };
