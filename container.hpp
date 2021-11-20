12526179339c40e20c23b5c2d221d1aba98e2e3a
aUser972 <alexeyefm@yandex.ru>
2021-11-20 21:05:39 +0300

    [A] improve container

--- container.hpp
+++ container.hpp
@@ -8,7 +8,7 @@ class Container
     T* arr;
     size_t size;
     size_t capacity;
-    Container(size_t size): size {size}, capacity {size * 2}
+    Container(size_t size, T value = T()): size {size}, capacity {size * 2}
     {        
         arr = new T[capacity];
     }
@@ -18,20 +18,28 @@ class Container
         T* newarr = reinterpret_cast<T*>(new uint8_t[ n*sizeof(T) ]);
         //placment new
         try {
-            for(size_t i; i < size; ++i)
-            {
-                new (newarr + i)T(arr[i]);
-            }
+            std::uninitialized_copy(arr, arr + size, newarr);
         }
         catch(...)
         {
-            for(size_t j=i-1; j >=0; --j)
-            {
-                (newarr + i)->~T();
-            }
             delete[] reinteret_cast<uint8_t>(newarr);
-            throw;
+            throw;    
         }
+        // try {
+        //     for(size_t i; i < size; ++i)
+        //     {
+        //         new (newarr + i)T(arr[i]);
+        //     }
+        // }
+        // catch(...)
+        // {
+        //     for(size_t j=0; j<i; ++qj)
+        //     {
+        //         (newarr + i)->~T();
+        //     }
+        //     delete[] reinteret_cast<uint8_t>(newarr);
+        //     throw;
+        // }
         for(size_t i=0; i < size; ++i)
         {
             (arr + i)->~T();
@@ -41,26 +49,32 @@ class Container
         capacity = n;
     }
 
-    void resize(size_t n, T value = T())
+    void resize(size_t n, const T& value = T())
     {        
         if(n > capacity) reserve(n);
         size = n;
+
+        try {
+            std::uninitialized_copy(arr, arr + size, newarr);
+        }
+        catch(...)
+        {
+            delete[] reinteret_cast<uint8_t>(newarr);
+            throw;    
+        }
 
         // insert default data
     }
 
     void push_back(const T& value)
     {
-        if (size >= capacity) resize(size*2);
-        try {
-            new (arr + size) value;
-        }
-        catch(...)
-        {
-            // Shoud I do something?
-            throw;
-        }
+        if (size == capacity) resize(size*2);
+        new (arr + size) T(value);
         ++size;        
     }
-    void pop_back() {};
+    void pop_back() 
+    {
+        --size;
+        (arr + size)->~T();
+    }
 };
