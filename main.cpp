f606f4da5bbca8d84585e79fd2c167115751f7d8
aUser972 <alexeyefm@yandex.ru>
2022-01-19 08:30:30 +0300

    [M] modofied tuple

--- main.cpp
+++ main.cpp
@@ -7,34 +7,24 @@
 #include <memory_resource>
 #include <stdio.h>
 
-#include "tmp_allocator.hpp"
-#include "shared_ptr.hpp"
-#include "weak_ptr.hpp"
-#include "allocator.hpp"
-#include "vector.hpp"
+// #include "tmp_allocator.hpp"
+// #include "shared_ptr.hpp"
+// #include "weak_ptr.hpp"
+// #include "allocator.hpp"
+// #include "vector.hpp"
 
-void observe(std::weak_ptr<int> weak) 
+template<typename ... Args>
+void  foo(Args& ...args)
 {
-    if (auto observe = weak.lock()) {
-        std::cout << "\tobserve() able to lock weak_ptr<>, value=" << "\n";
-    } else {
-        std::cout << "\tobserve() unable to lock weak_ptr<>\n";
-    }
+    std::cout << sizeof...(args) << '\n';
 }
- 
+
 int main()
 {
-    std::weak_ptr<int> weak;
-    std::cout << "weak_ptr<> not yet initialized\n";
-    observe(weak);
- 
-    {
-        auto shared = std::make_shared<int>(42);
-        weak = shared;
-        std::cout << "weak_ptr<> initialized with shared_ptr.\n";
-        observe(weak);
-    }
- 
-    std::cout << "shared_ptr<> has been destructed due to scope exit.\n";
-    observe(weak);
+    int x = 32;
+    double y = 144.0;
+    char c = 'c';
+    std::cout << sizeof(x) << '\n';
+    foo<int, double, char>(x, y, c);
+    return 0;
 }
--- tuple.hpp
+++ tuple.hpp
@@ -1,7 +1,23 @@
 #pragma once
 
+template<>
+class tuple<> {};
+
 template<typename... Args>
-class tuple
+class tuple {};
+
+template<typename Head, typename ... Tail>
+class tuple : tuple<Tail...>
 {
-    
+    typedef tuple<Tail...> basic_type;
+    typedef Head value_type;
+
+    Head head;
+public:
+    tuple(Head head, Tail ... tail): head{head}, tuple<Tail...>{tail...}
+    {
+        
+    }
+private:
+
 };
