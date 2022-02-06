90cf820384b22d05980e876b729425f5cb7054a7
aUser972 <alex.efimov311@gmail.com>
2022-02-06 13:03:56 +0300

    [A] add get function to tuple

--- main.cpp
+++ main.cpp
@@ -6,25 +6,33 @@
 #include <bitset>
 #include <memory_resource>
 #include <stdio.h>
+#include <tuple>
+#include <stdexcept>
+#include <tuple>
 
-// #include "tmp_allocator.hpp"
-// #include "shared_ptr.hpp"
-// #include "weak_ptr.hpp"
-// #include "allocator.hpp"
-// #include "vector.hpp"
+#include "tmp_allocator.hpp"
+#include "shared_ptr.hpp"
+#include "weak_ptr.hpp"
+#include "allocator.hpp"
+#include "vector.hpp"
+#include "tuple.hpp"
 
-template<typename ... Args>
-void  foo(Args& ...args)
+template<int I, typename... Args>
+std::tuple<Args...> func(Args... args)
 {
-    std::cout << sizeof...(args) << '\n';
+    std::cout << "I value is :" << I << std::endl;
+    return std::tuple<Args...>(args...);
 }
-
+ 
 int main()
 {
-    int x = 32;
-    double y = 144.0;
-    char c = 'c';
-    std::cout << sizeof(x) << '\n';
-    foo<int, double, char>(x, y, c);
+    // tuple<int, double, int> a(1, 3.3, 4);
+    // tuple<int, double, int> b(1, 14.1,  5);
+    // a = b;
+    // int x = 2;
+    // auto var = get<2>(a);
+    // std::cout << "2 Value is: " << var << std::endl;
+    auto a = func<45>(1, 2, 3);
+    std::cout << "Result: " << std::get<0>(a) << ' ' << std::get<1>(a) << ' ' << std::get<2>(a);
     return 0;
 }
--- tuple.hpp
+++ tuple.hpp
@@ -1,23 +1,62 @@
 #pragma once
-
-template<>
-class tuple<> {};
+#include <iostream>
 
 template<typename... Args>
-class tuple {};
+struct tuple;
 
-template<typename Head, typename ... Tail>
-class tuple : tuple<Tail...>
+template<typename Head, typename... Tail>
+struct tuple<Head, Tail...> : tuple<Tail...>
 {
     typedef tuple<Tail...> basic_type;
     typedef Head value_type;
 
+    
+
     Head head;
-public:
     tuple(Head head, Tail ... tail): head{head}, tuple<Tail...>{tail...}
     {
-        
+        std::cout << "Create tuple instance with type: " << typeid(*this).name() << '\n';
+        std::cout << "And head type: " << typeid(head).name() << '\n';
     }
-private:
-
+    tuple& operator=(const tuple other)
+    {
+        std::cout << "Call assignment operator\n";
+        this->head = other.head;
+        return *this;
+    }
+    // friend static value_type get(tuple<Head, Tail...> t);
 };
+
+template<>
+struct tuple<>
+{};
+
+template<int I, typename Head, typename... Args>
+struct getter
+{
+    typedef typename getter<I-1, Args...>::return_type return_type;
+    static return_type get(tuple<Head, Args...> t)
+    {
+        std::cout << "Second I is:" << I << std::endl;
+        return getter<I-1, Args...>::get(t);
+    }
+};
+
+template<typename Head, typename... Args>
+struct getter<0, Head, Args...>
+{
+    typedef typename tuple<Head, Args...>::value_type return_type;
+    static return_type get(tuple<Head, Args...> t)
+    {
+        std::cout << "Bottom" << std::endl;
+        return t.head;
+    }
+};
+
+template<int I, typename Head, typename... Args>
+typename getter<I, Head, Args...>::return_type
+get(tuple<Head, Args...> t)
+{
+    std::cout << "First I is:" << I << std::endl;
+    return getter<I, Head, Args...>::get(t);
+}
